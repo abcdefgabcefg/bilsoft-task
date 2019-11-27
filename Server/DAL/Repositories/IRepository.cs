8ac4a191174529b5d84b6ccc1490a16958477723
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-27 21:09:03 +0200

    Add repository

---
+++ Server/DAL/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DAL.Repositories
+{
+    public interface IRepository<TEntity> where TEntity: EntityBase
+    {
+        public List<TEntity> Get(int? skip = null, int? take = null, params Expression<Func<TEntity, object>>[] includes);
+    }
+}
---
+++ Server/DAL/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DAL.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : EntityBase
+    {
+        private DbSet<TEntity> _entities;
+
+        public Repository(DbContext context)
+        {
+            _entities = context.Set<TEntity>();
+        }
+
+        public List<TEntity> Get(int? skipCount = null, int? takeCount = null, params Expression<Func<TEntity, object>>[] includes)
+        {
+            var entities = Include(_entities, includes);
+
+            if (skipCount.HasValue)
+            {
+                entities = entities.Skip(skipCount.Value);
+            }
+
+            if (takeCount.HasValue)
+            {
+                entities = entities.Take(takeCount.Value);
+            }
+
+            return entities.ToList();
+        }
+
+        private IQueryable<TEntity> Include(IQueryable<TEntity> entities, IEnumerable<Expression<Func<TEntity, object>>> properties)
+        {
+            foreach (var property in properties)
+            {
+                entities = entities.Include(property);
+            }
+
+            return entities;
+        }
+    }
+}
