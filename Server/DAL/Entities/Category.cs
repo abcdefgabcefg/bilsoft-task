43c365ab0c6c5bbdd3c17e83b589a17304da7d48
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-28 00:26:59 +0200

    Fix circular loading products

--- Server/DAL/Entities/Category.cs
+++ Server/DAL/Entities/Category.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace DAL.Entities
+﻿namespace DAL.Entities
 {
     public class Category : EntityBase
     {
         public string Title { get; set; }
-
-        public List<Product> Products { get; set; }
     }
 }
