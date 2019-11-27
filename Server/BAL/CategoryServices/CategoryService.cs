c9b17d856aa952bd9a6d56ffefc6a942136ae98f
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-27 21:40:27 +0200

    Add category service

---
+++ Server/BAL/CategoryServices/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Entities;
+using DAL.UnitOfWork;
+using System.Collections.Generic;
+
+namespace BAL.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<Category> GetAll()
+        {
+            var categories = _unitOfWork.Categories.Get();
+
+            return categories;
+        }
+    }
+}
---
+++ Server/BAL/CategoryServices/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+
+namespace BAL.CategoryServices
+{
+    public interface ICategoryService
+    {
+        public List<Category> GetAll();
+    }
+}
