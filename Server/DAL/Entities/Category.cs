f1bdbdae51165c0d0244b5e2b45047f75386f44d
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-27 20:19:48 +0200

    Create entities

---
+++ Server/DAL/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class Category : EntityBase
+    {
+        public string Title { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Server/DAL/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAL.Entities
+{
+    public class Product : EntityBase 
+    {
+        public string Title { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
