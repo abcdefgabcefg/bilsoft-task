27b45b2293955eae7479d51b990d9b2a49d057d0
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-27 21:35:10 +0200

    Fix IUnitOfWork interface

--- Server/DAL/UnitOfWork/IUnitOfWork.cs
+++ Server/DAL/UnitOfWork/IUnitOfWork.cs
@@ -1,7 +1,14 @@
-﻿namespace DAL.UnitOfWork
+﻿using DAL.Entities;
+using DAL.Repositories;
+
+namespace DAL.UnitOfWork
 {
     public interface IUnitOfWork
     {
+        public IRepository<Product> Products { get; }
+        
+        public IRepository<Category> Categories { get; }
+
         public int Save();
     }
 }
