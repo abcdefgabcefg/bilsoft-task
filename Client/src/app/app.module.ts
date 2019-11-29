f6211668263c144a5f36763c07e4340a8ae53ec9
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-29 23:53:00 +0200

    Create basic create product component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Client/src/app/create-product/create-product.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Injectable } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { Category } from '../category';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+@Injectable()
+export class CreateProductComponent implements OnInit {
+  categories : Category[];
+  createProductForm: FormGroup;
+
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder) {
+      http
+      .get<Category[]>("https://localhost:44335/api/category")
+      .subscribe(categories => this.categories = categories);
+
+      this.createProductForm = formBuilder.group({
+        title: '',
+        category: ''
+      });
+   }
+
+  ngOnInit() {
+  }
+
+  create(product: any){
+    console.log(product);
+  }
+
+}
