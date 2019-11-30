c6eb63b84e65f831894607d8a75d64c6bc995a59
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-30 19:24:48 +0200

    Add validation for create product form

--- Client/src/app/create-product/create-product.component.ts
+++ Client/src/app/create-product/create-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { CategoryService } from '../category.service';
 import { ProductService } from '../product.service';
@@ -15,6 +15,9 @@ import { Category } from '../category';
 export class CreateProductComponent implements OnInit {
   categories : Category[];
   createProductForm: FormGroup;
+  get title(){
+    return this.createProductForm.get('title');
+  }
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,22 +36,28 @@ export class CreateProductComponent implements OnInit {
         selectedCategory = categories[0].id;
       }
       
-      this.createProductForm = this.formBuilder.group({
-        title: '',
-        category: [selectedCategory]
-      });
+      this.createProductForm = this.createForm(selectedCategory);
     });
 
-    this.createProductForm = this.formBuilder.group({
-      title: '',
-      category: ''
-    });
+    this.createProductForm = this.createForm('');
   }
 
-  create(product : any){
+  createProduct(productForm : FormGroup){
+    if(!productForm.valid){
+      this.title.markAllAsTouched();
+      return;
+    }
     this.productService
-      .create(product)
+      .create(productForm.value)
       .subscribe(() => this.eventService.productCreated.nofify());
   }
 
+  createForm(selectedCategory: string): FormGroup{
+    const createProductForm = this.formBuilder.group({
+      title: new FormControl('', Validators.required),
+      category: selectedCategory
+    });
+
+    return createProductForm;
+  }
 }
