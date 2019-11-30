d1b1b59c8f14096700f59bc69a6553780fc84d59
Oleksandr Riadynskyi <Oleksandr_Riadynskyi@epam.com>
2019-11-30 14:54:13 +0200

    Subscribe pagination to product creation

--- Client/src/app/pagination/pagination.component.ts
+++ Client/src/app/pagination/pagination.component.ts
@@ -1,29 +1,39 @@
 import { Component, OnInit, Output, Input, EventEmitter, Injectable } from '@angular/core';
 
 import { ProductService } from '../product.service'; 
+import { EventService } from '../event.service';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
+@Injectable()
 export class PaginationComponent implements OnInit{
   pages: number[];
   @Output() pageChanged : EventEmitter<number> = new EventEmitter();
   @Input() take: number;
 
-  constructor(private productService: ProductService) {
+  constructor(
+    private productService: ProductService,
+    private eventService: EventService) {
 
   }
 
   ngOnInit(){
-    this.productService.count().subscribe(productsCount => {
-      const pagesCount = Math.ceil(productsCount / this.take);
-      this.pages = [...Array(pagesCount).keys()].map(page => ++page);
-    });
+    this.getCount();
+
+    this.eventService.productCreated.subscribe(() => this.getCount());
    }
 
   changePage(page: number): void{
     this.pageChanged.emit(page);
+   }
+
+   getCount(): void{
+    this.productService.count().subscribe(productsCount => {
+      const pagesCount = Math.ceil(productsCount / this.take);
+      this.pages = [...Array(pagesCount).keys()].map(page => ++page);
+    });
    }
 }
